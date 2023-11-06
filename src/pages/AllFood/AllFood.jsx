1cc8bc6a94883a9893706b427dc567aad1edae8a
shakib2123 <mshakibalhasan21@gmail.com>
2023-11-06 16:43:36 +0600

    pagination method added

--- src/pages/AllFood/AllFood.jsx
+++ src/pages/AllFood/AllFood.jsx
@@ -5,17 +5,19 @@ import { PulseLoader } from "react-spinners";
 import Lottie from "lottie-react";
 import errorAnime from "../../assets/ErrorAnime.json";
 import Navbar from "../../components/Navbar/Navbar";
+import { useState } from "react";
 const AllFood = () => {
   const axios = useAxios();
-
+  const [page, setPage] = useState(1);
+  const limit = 9;
   const {
     data: foods,
     isLoading,
     isError,
   } = useQuery({
-    queryKey: ["allFoods"],
+    queryKey: ["allFoods", page],
     queryFn: async () => {
-      const res = await axios.get("/foods");
+      const res = await axios.get(`/foods?page=${page}&limit=${limit}`);
       return res.data;
     },
   });
@@ -35,14 +37,54 @@ const AllFood = () => {
       </div>
     );
   }
+  const totalFoods = foods.foodsCount;
+  const pages = Math.ceil(totalFoods / limit);
+  const pageOfNum = [...Array(pages).fill(0)];
+  const handlePrev = () => {
+    if (page > 1) {
+      setPage(page - 1);
+    }
+  };
+  const handleNext = () => {
+    if (page < pages) {
+      setPage(page + 1);
+    }
+  };
+  console.log(totalFoods);
 
   return (
     <div>
       <Navbar />
       <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 my-8">
-        {foods.map((food) => (
+        {foods?.result.map((food) => (
           <Foods key={food._id} food={food}></Foods>
         ))}
+      </div>
+      <div className="flex justify-center my-8">
+        <div className="join rounded-lg">
+          <button onClick={handlePrev} className="join-item btn">
+            «
+          </button>
+          {pageOfNum.map((item, index) => {
+            const pageNum = index + 1;
+            return (
+              <button
+                key={pageNum}
+                onClick={() => setPage(pageNum)}
+                className={
+                  page === pageNum
+                    ? "join-item btn btn-active"
+                    : "join-item btn"
+                }
+              >
+                {pageNum}
+              </button>
+            );
+          })}
+          <button onClick={handleNext} className="join-item btn">
+            »
+          </button>
+        </div>
       </div>
     </div>
   );
